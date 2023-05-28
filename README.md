3dd2eedfe7b8836acf0f1ced51bdb5eb9d58d69c
Kevin Bigler <kevin-bigler@users.noreply.github.com>
2023-05-28 08:08:07 -0700

    rename stuff, remove comment

---
+++ README.md
@@ -0,0 +1,3 @@
+# webpack + typescript base setup
+
+includes `npm run dev` script for Hot Module Replacement (HMR) development.
--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 function component() {
     const element = document.createElement('div')
 
-    // Lodash, now imported by this script
     element.innerHTML = ['yo', 'webpack'].join(' ')
 
     return element
 }
 
-document.title = "KB-SPOKE"
+document.title = "webpack-typescript-base-setup"
 document.body.appendChild(component())
